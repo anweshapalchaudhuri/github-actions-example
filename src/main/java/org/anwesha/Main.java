13656c743c6b42eab0eaed3b57fb30edf1735be0
Anwesha PalChaudhuri <anwesha.palchaudhuri@gmail.com>
2024-03-20 11:13:49 +0530

    code

---
+++ src/main/java/org/anwesha/Main.java
@@ -0,0 +1,26 @@
+package org.anwesha;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+@SpringBootApplication
+@RestController
+public class Main {
+
+    @GetMapping("/welcome")
+    public String welcome(){return "Git Actions POC 1.0";}
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+    }
+}
