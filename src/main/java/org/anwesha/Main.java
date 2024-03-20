38da4378f1769e4041b44de8ad58f3830216a479
Anwesha PalChaudhuri <anwesha.palchaudhuri@gmail.com>
2024-03-20 23:22:57 +0530

    changes and updates to pom and maven files

--- src/main/java/org/anwesha/Main.java
+++ src/main/java/org/anwesha/Main.java
@@ -1,5 +1,6 @@
 package org.anwesha;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,14 +14,16 @@ public class Main {
     @GetMapping("/welcome")
     public String welcome(){return "Git Actions POC 1.0";}
     public static void main(String[] args) {
+
+        SpringApplication.run(Main.class, args);
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+//        System.out.printf("Hello and welcome!");
+//
+//        for (int i = 1; i <= 5; i++) {
+//            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+//            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+//            System.out.println("i = " + i);
+//        }
     }
 }
